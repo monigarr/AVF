54fd8bcc3a4a58649149b219f099d430a795b175
Monica Peters <monigarr@yahoo.com>
2012-06-29 03:45:47 -0400

    added week2 folder

---
+++ week2/avf_wk1_crud/js/json.js
@@ -0,0 +1,98 @@
+//
+//    Full Sail University
+//    Visual Frameworks
+//    Monica Peters
+//    Web App Part 4
+//    Week 4 Project 4
+//    Due Thursday Feb. 23rd 2012
+//	  json.js
+
+// Add Test Data 
+// One object for each file type
+// book, document, music,  movie, pdf, word doc, audio, video
+// javascript lint passed June 27th 2012
+
+var json = 
+{
+	//object name
+	"book": 
+		{
+			//object properties and value
+			"mtype": ["", "book"],
+			"mname": ["", "Test Book"],
+			"mdate": ["", "2012-02-16"],
+			"mtopics": ["", "work"],
+			"mtags": ["", "worktag"],
+			"mcomments": ["","Comment about book goes here"]
+		},
+	"document": 
+		{
+			//object properties and value
+			"mtype": ["", "document"],
+			"mname": ["", "Test Document"],
+			"mdate": ["", "2009-05-20"],
+			"mtopics": ["", "personaltag"],
+			"mtags": ["", "gardening"],
+			"mcomments": ["","Comment about document is here."]
+		},
+	"music": 
+		{
+			//object properties and value
+			"mtype": ["", "music"],
+			"mname": ["", "Test Music"],
+			"mdate": ["", "1970-04-01"],
+			"mtopics": ["", "school"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about music goes here"]
+		},
+	"movie": 
+		{
+			//object properties and value
+			"mtype": ["", "movie"],
+			"mname": ["", "Test Movie"],
+			"mdate": ["", "1970-02-01"],
+			"mtopics": ["", "school"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about movie goes here"]
+		},
+	"pdf": 
+		{
+			//object properties and value
+			"mtype": ["", "pdf"],
+			"mname": ["", "Test Pdf"],
+			"mdate": ["", "1970-09-01"],
+			"mtopics": ["", "work"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about pdf goes here"]
+		},
+	"doc": 
+		{
+			//object properties and value
+			"mtype": ["", "doc"],
+			"mname": ["", "Test Doc"],
+			"mdate": ["", "1976-04-01"],
+			"mtopics": ["", "work"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about word doc goes here"]
+		},
+	"audio": 
+		{
+			//object properties and value
+			"mtype": ["", "audio"],
+			"mname": ["", "Test Audio"],
+			"mdate": ["", "1979-04-01"],
+			"mtopics": ["", "school"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about audio goes here"]
+		},
+	"video": 
+		{
+			//object properties and value
+			"mtype": ["", "video"],
+			"mname": ["", "Test Video"],
+			"mdate": ["", "1982-04-01"],
+			"mtopics": ["", "school"],
+			"mtags": ["", "schooltag"],
+			"mcomments": ["","Comment about video goes here"]
+		}
+};
